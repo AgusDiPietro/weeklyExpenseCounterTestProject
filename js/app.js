348151da5735609e48c2f6fe2dd9c53b0feaf31a
AgusDiP <agusdipietro@live.com.ar>
2022-05-31 20:56:53 +0200

    Agregando Clase presupuesto junto con la de interfazUsuario ed instanciando

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 //Variables y Selectores
 const formulario = document.querySelector("#agregar-gasto");
 const gastoListado = document.querySelector("#gastos ul")
-
+let presupuesto;
 
 // Eventos
 eventListeners();
@@ -9,17 +9,29 @@ function eventListeners(){
     document.addEventListener('DOMContentLoaded',preguntarPresupuesto);
 }
 
-
 //Clases
+class Presupuesto{
+    constructor(presupusto){
+        this.presupusto = Number(presupusto);
+        this.restante = Number(presupuesto);
 
+    }
+}
+class UI {
+
+}
 
 //Funciones
 function preguntarPresupuesto(){
     const presupuestoUsuario = prompt('¿Cúal es tu presupuesto?');
 
-    console.log(Number(presupuestoUsuario));
+   // console.log(Number(presupuestoUsuario));
 
     if (presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario || presupuestoUsuario <= 0)) {
         window.location.reload(); // Hace que se recargue la página.
     }
+
+    //Una vez validado el presupuesto
+    presupusto = new Presupuesto(presupuestoUsuario);
+
 }
